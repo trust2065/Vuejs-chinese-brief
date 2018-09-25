4b3da1ecd246a358779229bc0d4d09022148f280
Choco Li <cli@modernstar.com>
2018-09-25 16:25:23 +1000

    hello world with nav, 2 pages

---
+++ vueCLI/README.md
@@ -0,0 +1,3 @@
+## 部分內容參考六角學院Vue CLI示範教學
+
+https://www.youtube.com/watch?v=3ypel9_VtmU
--- vueCLI/myProject/src/App.vue
+++ vueCLI/myProject/src/App.vue
@@ -1,5 +1,26 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active pr-1">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/H2">Home 2</router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -7,13 +28,15 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
